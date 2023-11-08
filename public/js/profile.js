e2cffc96f2b40a6d0b1b93a5354ffa4b2f58ede4
TomasRullan <tomasrullan@yahoo.com>
2023-11-08 12:40:42 -0500

    fixes

--- public/js/profile.js
+++ public/js/profile.js
@@ -45,4 +45,11 @@ const newFormHandler = async (event) => {
   document
     .querySelector('.project-list')
     .addEventListener('click', delButtonHandler);
+
+    Handlebars.registerHelper("link", function(text, url) {
+      var url = Handlebars.escapeExpression(url),
+          text = Handlebars.escapeExpression(text)
+          
+     return new Handlebars.SafeString("<a href='" + url + "'>" + text +"</a>");
+});
   
